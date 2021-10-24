473e5ae6b324f0941fc74b43423e1d3cb3c45708
lyusk8 <lyusk8@gmail.com>
2021-10-24 13:08:16 -0300

    criado projeto

---
+++ src/main/kotlin/com/letscode/msauthorization/MsAuthorizationApplication.kt
@@ -0,0 +1,11 @@
+package com.letscode.msauthorization
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsAuthorizationApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsAuthorizationApplication>(*args)
+}
---
+++ src/test/kotlin/com/letscode/msauthorization/MsAuthorizationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.letscode.msauthorization
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsAuthorizationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
